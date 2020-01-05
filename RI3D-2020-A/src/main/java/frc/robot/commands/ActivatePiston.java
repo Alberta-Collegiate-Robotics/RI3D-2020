cfcc9d9dbaa46e15534e297125465e2669ecc823
HN67 <hn67allardkids@gmail.com>
2020-01-04 23:27:42 -0700

    Create ActivatePiston command, similar to ActivateMotor

---
+++ RI3D-2020-A/src/main/java/frc/robot/commands/ActivatePiston.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.PistonSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class ActivatePiston extends CommandBase {
+
+	private final PistonSubsystem subsystem;
+
+	/**
+	 * Creates a new ActivatePiston Command
+	 * Activates the given Piston subsystem until the command is interrupted
+	 *
+	 * @param subsystem The subsystem used by this command.
+	 */
+	public ActivatePiston(PistonSubsystem subsystem) {
+		// Reference subsystem
+		this.subsystem = subsystem;
+		// Use addRequirements() here to declare subsystem dependencies.
+		addRequirements(subsystem);
+	}
+
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		// Activate the piston
+		this.subsystem.setState(true);
+	}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {}
+
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		// Deactivate the piston
+		this.subsystem.setState(false);
+	}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
+}
