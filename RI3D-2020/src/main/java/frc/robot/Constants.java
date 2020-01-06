0cd11ffbe4ee815afa510508aac65647bde1666d
HN67 <hn67allardkids@gmail.com>
2020-01-05 19:57:21 -0700

    Cleanup solenoid ports and start camera capture

--- RI3D-2020/src/main/java/frc/robot/Constants.java
+++ RI3D-2020/src/main/java/frc/robot/Constants.java
@@ -35,10 +35,8 @@ public final class Constants {
     public static final int intakeMotorPort = 1;
     public static final int hopperMotorPort = 2;
 
-    public static final int pistonPort = -1;
-
     public static final int pistonSolenoidPortA = 0;
-    public static final int pistonSolenoidPortB = 0;
+    public static final int pistonSolenoidPortB = 1;
 
     //public static final int controlPanelPort = 0;
 
--- RI3D-2020/src/main/java/frc/robot/RobotContainer.java
+++ RI3D-2020/src/main/java/frc/robot/RobotContainer.java
@@ -33,6 +33,8 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.VictorSP;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.cameraserver.CameraServer;
+
 //import edu.wpi.first.wpilibj.Solenoid;
 
 /**
@@ -132,6 +134,9 @@ public class RobotContainer {
 		//this.controlPanelSubsystem = new MotorSubsystem(this.controlPanelMotor);
 
 		this.exampleSubsystem = new ExampleSubsystem();
+
+		// Start camera server
+		CameraServer.getInstance().startAutomaticCapture();
 
 		// Setup autocommand
 		//Command driveForward = new ActivateArcadeDrive(this.driveTrainSubsystem, 1.0, 0);
