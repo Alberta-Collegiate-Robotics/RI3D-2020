448c9e46f8991ef1a732b494551caad712515b83
Kappabyte <avery1516@gmail.com>
2020-01-05 19:23:57 -0700

    Change to DoubleSolenoid

--- RI3D-2020/src/main/java/frc/robot/commands/TogglePiston.java
+++ RI3D-2020/src/main/java/frc/robot/commands/TogglePiston.java
@@ -32,7 +32,7 @@ public class TogglePiston extends CommandBase {
 	// Called when the command is initially scheduled.
 	@Override
 	public void initialize() {
-		this.subsystem.setState(!this.subsystem.getState());
+		this.subsystem.setState(!this.subsystem.getBooleanState());
 	}
 
 	// Called every time the scheduler runs while the command is scheduled.
